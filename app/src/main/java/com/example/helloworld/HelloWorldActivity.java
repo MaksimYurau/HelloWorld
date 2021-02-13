a406d8e68997c6eef0f9787fef10625e6cfc4ef8
Maksim Yurau <2004softwaredeveloper@gmail.com>
2021-02-13 22:14:55 +0300

    Project update. Added activity_hello_world.xml. Updated HelloWorldActivity.java.

--- app/src/main/java/com/example/helloworld/HelloWorldActivity.java
+++ app/src/main/java/com/example/helloworld/HelloWorldActivity.java
@@ -8,5 +8,6 @@ public class HelloWorldActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_world);
     }
 }
