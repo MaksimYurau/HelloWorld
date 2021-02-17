936bef3d5d09792bc06bf4aefbcfce1cbdba2b20
Maksim Yurau <2004softwaredeveloper@gmail.com>
2021-02-17 20:22:21 +0300

    Project update. Updated HelloUsernameActivity.java. Updated activity_hello_username.xml.

--- app/src/main/java/com/example/helloworld/HelloUsernameActivity.java
+++ app/src/main/java/com/example/helloworld/HelloUsernameActivity.java
@@ -2,6 +2,9 @@ package com.example.helloworld;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class HelloUsernameActivity extends Activity {
 
@@ -9,5 +12,18 @@ public class HelloUsernameActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello_username);
+
+        final EditText nameEditText;
+        final TextView messageText;
+        Button btnSubmit;
+
+        nameEditText = findViewById(R.id.input_name);
+        messageText = findViewById(R.id.message_text);
+        btnSubmit = findViewById(R.id.btn_submit);
+
+        btnSubmit.setOnClickListener(v -> {
+            String name = nameEditText.getText().toString();
+            messageText.setText("Привет," + name);
+        });
     }
 }
