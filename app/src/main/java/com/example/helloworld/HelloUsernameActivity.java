7cbccac612bdb18cf99edcfe9df35d6b59259ce4
Maksim Yurau <2004softwaredeveloper@gmail.com>
2021-02-19 22:15:50 +0300

    Project update. Updated HelloUsernameActivity.java. Updated HelloWorldActivity.java. Updated MainActivity.java.

--- app/src/main/java/com/example/helloworld/HelloUsernameActivity.java
+++ app/src/main/java/com/example/helloworld/HelloUsernameActivity.java
@@ -2,15 +2,20 @@ package com.example.helloworld;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import static com.example.helloworld.BuildConfig.LOG;
+import static com.example.helloworld.BuildConfig.LOG_TAG;
+
 public class HelloUsernameActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        if (LOG) Log.d(LOG_TAG, getClass().getSimpleName() + ".onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello_username);
 
@@ -34,26 +39,31 @@ public class HelloUsernameActivity extends Activity {
 
     @Override
     protected void onStart() {
+        if (LOG) Log.d(LOG_TAG, getClass().getSimpleName() + ".onStart");
         super.onStart();
     }
 
     @Override
     protected void onStop() {
+        if (LOG) Log.d(LOG_TAG, getClass().getSimpleName() + ".onStop");
         super.onStop();
     }
 
     @Override
     protected void onResume() {
+        if (LOG) Log.d(LOG_TAG, getClass().getSimpleName() + ".onResume");
         super.onResume();
     }
 
     @Override
     protected void onPause() {
+        if (LOG) Log.d(LOG_TAG, getClass().getSimpleName() + ".onPause");
         super.onPause();
     }
 
     @Override
     protected void onDestroy() {
+        if (LOG) Log.d(LOG_TAG, getClass().getSimpleName() + ".onDestroy");
         super.onDestroy();
     }
 }
--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.helloworld;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+
+import static com.example.helloworld.BuildConfig.LOG;
+import static com.example.helloworld.BuildConfig.LOG_TAG;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        if (LOG) Log.d(LOG_TAG, getClass().getSimpleName() + ".onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -18,26 +23,31 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
     @Override
     protected void onStart() {
+        if (LOG) Log.d(LOG_TAG, getClass().getSimpleName() + ".onStart");
         super.onStart();
     }
 
     @Override
     protected void onStop() {
+        if (LOG) Log.d(LOG_TAG, getClass().getSimpleName() + ".onStop");
         super.onStop();
     }
 
     @Override
     protected void onResume() {
+        if (LOG) Log.d(LOG_TAG, getClass().getSimpleName() + ".onResume");
         super.onResume();
     }
 
     @Override
     protected void onPause() {
+        if (LOG) Log.d(LOG_TAG, getClass().getSimpleName() + ".onPause");
         super.onPause();
     }
 
     @Override
     protected void onDestroy() {
+        if (LOG) Log.d(LOG_TAG, getClass().getSimpleName() + ".onDestroy");
         super.onDestroy();
     }
 
